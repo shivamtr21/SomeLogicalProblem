4483555a9e3547cba94f3ec941036ce21b5ce99c
shivamtr21 <shivamtripathi26297@gmail.com>
2023-03-15 15:42:19 +0530

    find the sum of harmonic series

---
+++ HarmonicSum.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipCoin
+{
+    public class HarmonicSum
+
+    {
+        public static void CheckSum()
+
+        {
+            
+            Console.WriteLine("Enter the number for harmonic sum");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            double sum = 0.0;
+
+            if (N == 0)
+            {
+                Console.WriteLine("Please enter a valid number");
+            }
+
+            else
+            {
+                for (int i=1; i <=N; i++)
+                {
+                    sum +=( 1.0 / i);
+                    Console.WriteLine("The " + i + "th harmonic number is : ");
+                    Console.WriteLine("1 /" + i + " = " + sum);
+
+                }
+                
+            }
+            
+        }
+    }
+}
