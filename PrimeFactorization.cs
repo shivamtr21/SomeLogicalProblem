dcea16ad5e0acb8253e257bf1b31869d590bd686
shivamtr21 <shivamtripathi26297@gmail.com>
2023-03-15 15:41:01 +0530

    find the prime Factorization

---
+++ PrimeFactorization.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipCoin
+{
+    public class PrimeFactorization
+    {
+        public static void NPrimeFactor()
+        {
+            Console.WriteLine("Enter the value for prime factorization :");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            for (int i=2; i*i <N; i++)
+            {
+                while (N % i == 0)
+                {
+                    Console.WriteLine(i);
+                    N /= i;
+                }
+            }
+            if (N > 2)
+            {
+                Console.WriteLine(N);
+            }
+        }
+    }
+}
