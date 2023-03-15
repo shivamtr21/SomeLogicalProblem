b3b50869b0c188311bc1e1a7ebf01733429fa7b5
shivamtr21 <shivamtripathi26297@gmail.com>
2023-03-15 15:38:15 +0530

    Welcome to the logical

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FlipCoin
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the logical program");
+            //  CoinFlip.FlipTheCoin();
+            //CheckPrime.NumberIsPrime();
+            //NTimesOfPrimeNumber.CheckPrimeOrNot();
+            //CheckLeapYear.IsLeapYearOrNot();
+            // HarmonicSum.CheckSum();
+            //PrimeFactorization.NPrimeFactor();
+        }
+    }
+}
